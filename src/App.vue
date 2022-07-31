092a8b97a7f3290097d2f94e090e8cd5274b9be5
zhangyueping <1107362767@qq.com>
2022-07-31 13:43:43 +0800

    项目初始化-引入Vant组件库

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <!-- 路由的出口 -->
     <router-view />
+
   </div>
 </template>
 <script>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+// 加载Vant核心组件库
+import Vant from 'vant'
+// 加载vant全局样式
+import 'vant/lib/index.css'
+
+// 加载inconfont
+import '@/assets/iconfont/iconfont.css'
 // 加载全局样式文件
 import '@/styles/index.less'
+
+// 注册使用Vant组件库
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
